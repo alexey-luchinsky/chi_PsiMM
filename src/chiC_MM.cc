cace9893c25fd33cc2443b933c3deb44a3704878
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-08-26 14:40:18 -0400

    compiles

--- src/chiC_MM.cc
+++ src/chiC_MM.cc
@@ -28,7 +28,7 @@ using namespace std;
 EvtGen *myGenerator;
 
 
-void init_evtgen(int n=3)
+void init_evtgen()
 {
   EvtParticle* parent(0);
   EvtRandomEngine* eng = 0;
@@ -74,56 +74,78 @@ void write_histogram_to_file(TH1F &histogram, string file_name) {
 
 int main(int argc, char const *argv[])
 {
-  cout<<"Format: evtgen.exe [N=3]"<<endl;
-  int N=3;
-  if(argc>1) N=atoi(argv[1]);
-  if(N !=3 && N!=5) {
-    cout<<"N="<<N<<" is not realized yet"<<endl;
-    return -1;
-  }
-  cout<<"N="<<N<<endl;
-  TFile file(("../src/evtgen/omegaQQ_"+to_string(N)+"pi.root").c_str(),"RECREATE");
-  TNtuple tup("tup","tup","Q2");
+  cout<<"Format: ./chic_all.exe inParticle [nEv=1e6] [decay_file=my_decay.dec]"<<endl;
+   if(argc<2) {
+     cout<<"Wrong number of arguments!"<<endl;
+     return 1;
+   };
+   cout<<" Decaying particle "<<argv[1]<<endl;
 
-  init_evtgen(N);
-  static EvtId CHI = EvtPDL::getId("Omega_bb+");
+   int nEvents(1e6);
+   if(argc>2) nEvents=(int)atof(argv[2]);
+   cout<<" nEvents="<<nEvents<<endl;
+
+
+
+   char *decay_file;
+//   if(argc>3) decay_file=argv[3];
+//   else decay_file=(char*)"my_decay.dec";
+  decay_file=(char*)"my_decay.dec";
+   cout<<" decay_file = "<<decay_file<<endl;
+
+  init_evtgen();
+
   EvtParticle* parent(0);
-  int nEv=1e4;
 
-  for(int iEv=0; iEv<nEv; ++iEv)
-  {
-    if( iEv % (nEv/10) == 0) cout<<"iEv="<<iEv<<endl;
+  static EvtId CHI = EvtPDL::getId(std::string(argv[1]));
+
+
+  string outName=string("root_")+string(argv[1])+string(".root");
+  TFile file(outName.c_str(),"RECREATE");
+  TNtuple tup("tup","tup","id:q2:m2PsiK1:m2PsiK2:cosThEE:Mchi");
+  // Loop to create nEvents, starting from an Upsilon(4S)
+  int i;
+  for (i = 0; i < nEvents; i++) {
+    if(i%(nEvents/10)==0) {
+      cout<<"========= "<<(int)(100.*i/nEvents)<<" % =========="<<endl;
+    };
     // Set up the parent particle
     EvtVector4R pInit(EvtPDL::getMass(CHI), 0.0, 0.0, 0.0);
     parent = EvtParticleFactory::particleFactory(CHI, pInit);
     parent->setDiagonalSpinDensity();
+
     // Generate the event
     myGenerator->generateDecay(parent);
-    EvtVector4R pOout = parent->getDaug(0)->getP4Lab();
-    double Q2=(pInit-pOout).mass2();
-    tup.Fill(Q2);
+    // save the results
+    EvtVector4R pPsi = parent->getDaug(0)->getP4Lab();
+    EvtVector4R k1=parent->getDaug(1)->getP4Lab();
+    EvtVector4R k2=parent->getDaug(2)->getP4Lab();
+    EvtVector4R k = k1+k2;
+    EvtVector4R Ptot=pPsi+k1+k2;
+    double Q2=k*k;
+    double m2PsiK1 = (pPsi+k1)*(pPsi+k1);
+    double m2PsiK2 = (pPsi+k2)*(pPsi+k2);
+    double cosThEE=k.get(3)/k.d3mag();
+    tup.Fill(parent->getPDGId(), Q2, m2PsiK1, m2PsiK2, cosThEE, sqrt(Ptot*Ptot));
+    if(i<0) {
+      cout<<"(* debug print at i="<<i<<"========== *)"<<endl;
+      cout<<" pPsi="<<pPsi<<endl;
+      cout<<" Mpsi="<<sqrt(pPsi*pPsi)<<endl;
+      cout<<" k1="<<k1<<endl;
+      cout<<" mmu="<<sqrt(k1*k1)<<endl;
+      cout<<" k2="<<k2<<endl;
+      cout<<" mmu="<<sqrt(k2*k2)<<endl;
+      cout<<" Ptot="<<Ptot<<endl;
+      cout<<" Q2="<<(k1+k2)*(k1+k2)<<endl;
+      cout<<" cosThEE="<<cosThEE<<endl;
+    };
+    parent->deleteTree();
 
+  }
 
-    if(iEv<0)
-    {
-      cout<<pOout<<endl;
-      cout<<"MOout="<<pOout.mass()<<endl;
-      cout<<"==============="<<endl;
-    }
-
-
-
-
-  };
+//  delete eng;
   tup.Write();
-  TH1F hQ2("hQ2","hQ2",50,tup.GetMinimum("Q2"),tup.GetMaximum("Q2"));
-  hQ2.Sumw2();
-  tup.Project("hQ2","Q2");
-  write_histogram_to_file(hQ2, "../src/evtgen/hQ2_"+to_string(N)+".txt");
   file.Save();
-
-
-
 
   return 0;
 }
