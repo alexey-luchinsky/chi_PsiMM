4bd5ed1df997356942e84ec65911a7f3b05d5304
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-10-25 15:52:23 -0400

    histogram regouping added

--- root_files/read_data.C
+++ root_files/read_data.C
@@ -1,20 +1,22 @@
 TFile *_file;
 
-void write_histogram_to_file(TH1F &histogram, string file_name) {
+void write_histogram_to_file(TH1F &histogram, string file_name, int ngroup=1) {
     const char *__file_name__ = file_name.c_str();
     remove(__file_name__);
     ofstream file;
         cout<<"Extporting histogram "<<file_name<<endl;
     file.open(__file_name__);
+    histogram.Rebin(ngroup);
     for (int i = 1; i <= histogram.GetNbinsX(); i++)
         file << setiosflags(ios::scientific) << histogram.GetBinCenter(i) <<
         " " << setiosflags(ios::scientific) << histogram.GetBinContent(i) / histogram.GetBinWidth(i) <<
         " " << setiosflags(ios::scientific) << histogram.GetBinError(i) / histogram.GetBinWidth(i) << endl;
     file.close();
-    cout<<"\t integral="<<histogram.Integral()<<endl;
+    cout<<"\t integral="<<histogram.Integral()<<" nBins="<<histogram.GetNbinsX()<<endl;
 }
 
-void saveData(int id, string postfix) {
+
+void saveData(int id, string postfix, int ngroup=1) {
   string query;
   string name;
   switch(id) {
@@ -31,23 +33,23 @@ void saveData(int id, string postfix) {
   cout<<" query="<<query<<endl;
   if(tup->GetEntries(query.c_str())>0) {
     tup->Project("hMchi0","Mchi",query.c_str());
-    write_histogram_to_file(*hMchi0,("hst/hM_"+name+".hst").c_str());
+    write_histogram_to_file(*hMchi0,("hst/hM_"+name+".hst").c_str(),ngroup);
     hMchi0->Delete();
-    
+
     tup->Project("hQ20","q2",query.c_str());
-    write_histogram_to_file(*hQ20,("hst/hQ2_"+name+".hst").c_str());
+    write_histogram_to_file(*hQ20,("hst/hQ2_"+name+".hst").c_str(),ngroup);
     hQ20->Delete();
-    
+
     tup->Project("hCos1","cosThEE",query.c_str());
-    write_histogram_to_file(*hCos1,("hst/hCos_"+name+".hst").c_str());
+    write_histogram_to_file(*hCos1,("hst/hCos_"+name+".hst").c_str(),ngroup);
     hCos1->Delete();
-    
+
     tup->Project("hm2PsiK1","m2PsiK1",query.c_str());
-    write_histogram_to_file(*hm2PsiK1,("hst/hm2PsiK1_"+name+".hst").c_str());
+    write_histogram_to_file(*hm2PsiK1,("hst/hm2PsiK1_"+name+".hst").c_str(),ngroup);
     hm2PsiK1->Delete();
-    
+
     tup->Project("hm2K1KK1","m2K1KK1",query.c_str());
-    write_histogram_to_file(*hm2K1KK1,("hst/hm2K1KK1_"+name+".hst").c_str());
+    write_histogram_to_file(*hm2K1KK1,("hst/hm2K1KK1_"+name+".hst").c_str(),ngroup);
     hm2K1KK1->Delete();
   }
   else {
@@ -59,13 +61,12 @@ void saveData(int id, string postfix) {
 
 TChain *chainc, *chainb;
 
-read_data_chi(string fff="max", string postfix="") {
+read_data_chi(string fff="max", string postfix="",int ngroup=1) {
   TChain chain("tup");
   chain.Add(("root_chi*_"+fff+".root").c_str());
   cout<<tup->GetEntries()<<" entries"<<endl;
   int idChi0=10441, idChi1=20443, idChi2=445;
   int idChiB0=10551, idChiB1=20553, idChiB2=555;
-  saveData(idChi0,postfix);  saveData(idChi1,postfix); saveData(idChi2,postfix);
-  saveData(idChiB0,postfix); saveData(idChiB1,postfix);saveData(idChiB2,postfix);
+  saveData(idChi0,postfix,ngroup);  saveData(idChi1,postfix,ngroup); saveData(idChi2,postfix,ngroup);
+  saveData(idChiB0,postfix,ngroup); saveData(idChiB1,postfix,ngroup);saveData(idChiB2,postfix,ngroup);
 }
-
