69bc5ff80a027cdea2f0d3c92f2406bfcdc111b7
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-11-08 15:56:08 -0500

    stat info printed

--- src/test.cc
+++ src/test.cc
@@ -22,9 +22,102 @@
 #include "TFile.h"
 #include "TNtuple.h"
 #include "TLorentzVector.h"
-
+#include "TH1F.h"
 using namespace std;
 
+void print_mean(TNtuple *tup, string var);
+
 int main(int argc, char** argv) {
-    cout<<"test"<<endl;
+    // ======== READ params ==========================
+    cout << "Format: ./test.exe inParticle [nEv=1e6] [decay_file=my_decay.dec] [postfix=]" << endl;
+    if (argc < 2) {
+        cout << "Wrong number of arguments!" << endl;
+        return 1;
+    };
+    cout << " Decaying particle " << argv[1] << endl;
+
+    int nEvents(1e6);
+    if (argc > 2) nEvents = (int) atof(argv[2]);
+    cout << " nEvents=" << nEvents << endl;
+
+
+    char *decay_file;
+    if (argc > 3) decay_file = argv[3];
+    else decay_file = (char*) "../src/my.dec";
+    cout << " decay_file = " << decay_file << endl;
+
+    char *postfix;
+    if (argc > 4) postfix = argv[4];
+    else postfix = (char*) "";
+    cout << "postfix=" << postfix << endl;
+
+    // ======== INIT ==========================
+    EvtParticle * parent(0);
+    EvtRandomEngine* eng = 0;
+#ifdef EVTGEN_CPP11
+    // Use the Mersenne-Twister generator (C++11 only)
+    eng = new EvtMTRandomEngine();
+#else
+    eng = new EvtSimpleRandomEngine();
+#endif
+
+    EvtRandom::setRandomEngine(eng);
+
+
+    EvtAbsRadCorr* radCorrEngine = 0;
+
+    std::list<EvtDecayBase*> extraModels;
+    EvtModel &modelist = EvtModel::instance();
+    modelist.registerModel(new EvtSVP_mm);
+    modelist.registerModel(new EvtVVP_mm);
+    modelist.registerModel(new EvtTVP_mm);
+
+    modelist.registerModel(new EvtSVP_);
+
+    EvtGen *myGenerator = new EvtGen(decay_file, "../src/evt.pdl", eng,
+            radCorrEngine, &extraModels);
+
+
+
+    static EvtId CHI = EvtPDL::getId(std::string(argv[1]));
+
+    double e1,pz1,e2,pz2,e3,pz3;
+    EvtVector4R p1,p2,p3;
+    string outName = string("test_") + string(argv[1]) + postfix + string(".root");
+    TFile file(outName.c_str(), "RECREATE");
+    TNtuple tup("tup", "tup", "e1:pz1:e2:pz2:e3:pz3");
+
+    for (int iEv = 0; iEv < nEvents; iEv++) {
+//        if (iEv % (nEvents / 10) == 0) {
+//            cout << "========= " << (int) (100. * iEv / nEvents) << " % ==========" << endl;
+//        };
+        // Set up the parent particle
+        EvtVector4R pInit(EvtPDL::getMass(CHI), 0.0, 0.0, 0.0);
+        parent = EvtParticleFactory::particleFactory(CHI, pInit);
+        parent->setDiagonalSpinDensity();
+        // Generate the event
+        myGenerator->generateDecay(parent);
+        p1=parent->getDaug(0)->getP4Lab();
+        p2=parent->getDaug(1)->getP4Lab();
+        if(parent->getNDaug()>2)
+            p3=parent->getDaug(2)->getP4Lab();
+        tup.Fill(p1.get(0),abs(p1.get(3)),p2.get(0),abs(p2.get(3)),p3.get(0),abs(p3.get(3)));
+        parent->deleteTree();
+    }
+
+    tup.Write();
+    file.Save();
+    cout<<"================"<<endl;
+    print_mean(&tup,"e1");print_mean(&tup,"pz1");
+    print_mean(&tup,"e2");print_mean(&tup,"pz2");
+    print_mean(&tup,"e3");print_mean(&tup,"pz3");
+    return 0;
+}
+
+void print_mean(TNtuple *tup, string var) {
+    const char *cvar=var.c_str();
+    TH1F *hist=new TH1F(cvar,cvar,10,tup->GetMinimum(cvar),tup->GetMaximum(cvar));
+    tup->Project(cvar,cvar);
+    cout<<"<"<<var<<">="<<hist->GetMean()<<endl;
+    delete hist;
 }
