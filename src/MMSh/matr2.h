360db9b796706f961a576c94009d1509bb2b2d7d
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-11-16 17:42:21 -0500

    matr2 in separate file

---
+++ src/MMSh/matr2.h
@@ -0,0 +1,19 @@
+/* 
+ * File:   matr2.h
+ * Author: luchinsky
+ *
+ * Created on November 16, 2017, 5:29 PM
+ */
+
+#ifndef MATR2_H
+#define	MATR2_H
+extern double mmu, Mpsi;
+extern double Mchi0, Mchi1, Mchi2;
+extern double const PI;
+
+double matr2_0(double pPsi[4], double k1[4], double k2[4]);
+double matr2_1(double pPsi[4], double k1[4], double k2[4]);
+double matr2_2(double pPsi[4], double k1[4], double k2[4]);
+
+#endif	/* MATR2_H */
+
