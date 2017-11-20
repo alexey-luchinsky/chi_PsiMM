8021f259746258643565deb9f8a3e647ff9da56e
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-11-20 17:25:53 -0500

    L^mu L_mu OK

--- src/matr2_pol/gen_pol.cc
+++ src/matr2_pol/gen_pol.cc
@@ -8,28 +8,57 @@
 #include "EvtGenBase/EvtVectorParticle.hh"
 #include "EvtGenBase/EvtDiracParticle.hh"
 #include "EvtGenBase/EvtDiracSpinor.hh"
+#include "EvtGenBase/EvtId.hh"
 
 using namespace std;
 
 int main(void) {
     cout << "gen_pol" << endl;
-    int nOut = 3;
-    double masses[30];
-    masses[0] = 3.1;
-    masses[1] = 0.105;
-    masses[2] = 0.105;
-    double mp = 3.55;
-    EvtVector4R p[30];
     EvtRandomEngine *eng = new EvtSimpleRandomEngine();
     EvtRandom::setRandomEngine(eng);
+    EvtGen *myGenerator = new EvtGen("../src/chic_matr2.dec", "../src/evt.pdl", eng);
+    int nOut = 3;
+    double masses[30];
+
+    double mPsi = EvtPDL::getMass(EvtPDL::getId("J/psi")), mmu = EvtPDL::getMass(EvtPDL::getId("mu+"));
+    masses[0] = mPsi;
+    masses[1] = mmu;
+    masses[2] = mmu;
+    double mp = 3.55;
+    EvtVector4R p[30];
+
+
+    EvtScalarParticle *chi0 = new EvtScalarParticle();
+    EvtVectorParticle *psi = new EvtVectorParticle();
+    EvtDiracParticle *mu1 = new EvtDiracParticle(), *mu2 = new EvtDiracParticle();
+    EvtVector4R pPsi, k1, k2, Ptot;
     for (int iEv = 0; iEv < 10; ++iEv) {
         cout << "======== event " << iEv << "=========" << endl;
         EvtGenKine::PhaseSpace(nOut, masses, p, mp);
-        EvtVector4R pPsi = p[0], k1 = p[1], k2 = p[2];
-        cout << " pPsi=" << pPsi << " m=" << pPsi.mass() << endl;
-        cout << " k1=" << k1 << " m=" << k1.mass() << endl;
-        cout << " k2=" << k2 << " m=" << k2.mass() << endl;
-        EvtVector4R Ptot = pPsi + k1 + k2;
-        cout << "Ptot=" << Ptot << endl;
+        pPsi = p[0];
+        psi->init(EvtPDL::getId("J/psi"), pPsi);
+        k1 = p[1];
+        mu1->init(EvtPDL::getId("mu+"), k1);
+        k2 = p[2];
+        mu2->init(EvtPDL::getId("mu-"), k2);
+        double amp2 = 0;
+        for (int iMplus = 0; iMplus < mu1->getSpinStates(); ++iMplus) {
+            EvtDiracSpinor spMplus = mu1->spParent(iMplus);
+            for (int iMminus = 0; iMminus < mu2->getSpinStates(); ++iMminus) {
+                EvtDiracSpinor spMminus = mu2->spParent(iMminus);
+                EvtVector4C epsGamma = EvtLeptonVCurrent(spMminus, spMplus);
+                amp2 += real(epsGamma * epsGamma.conj());
+            };
+        };
+        if (iEv <10) {
+            cout << " pPsi=" << pPsi << " m=" << pPsi.mass() << endl;
+            cout << " k1=" << k1 << " m=" << k1.mass() << endl;
+            cout << " k2=" << k2 << " m=" << k2.mass() << endl;
+            Ptot = pPsi + k1 + k2;
+            cout << "Ptot=" << Ptot << endl;
+            cout << " amp2=" << amp2 << endl;
+            double th = -8 * (k1 * k2 + 2 * mmu * mmu);
+            cout << " amp2/th=" << amp2 / th << endl;
+        };
     }
 }
