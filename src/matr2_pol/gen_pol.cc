563857d521c03ca5fb4d1c7f4885770a5b3bccbd
alexey.luchinsky@gmail.com <alexey.luchinsky@gmail.com>
2017-11-21 10:05:29 -0500

    epsGamma*epsPsi OK

--- src/matr2_pol/gen_pol.cc
+++ src/matr2_pol/gen_pol.cc
@@ -42,22 +42,26 @@ int main(void) {
         k2 = p[2];
         mu2->init(EvtPDL::getId("mu-"), k2);
         double amp2 = 0;
-        for (int iMplus = 0; iMplus < mu1->getSpinStates(); ++iMplus) {
-            EvtDiracSpinor spMplus = mu1->spParent(iMplus);
-            for (int iMminus = 0; iMminus < mu2->getSpinStates(); ++iMminus) {
-                EvtDiracSpinor spMminus = mu2->spParent(iMminus);
-                EvtVector4C epsGamma = EvtLeptonVCurrent(spMminus, spMplus);
-                amp2 += real(epsGamma * epsGamma.conj());
+        for (int iPsi = 0; iPsi < psi->getSpinStates(); ++iPsi) {
+            EvtVector4C epsPsi=psi->epsParent(iPsi);
+            for (int iMplus = 0; iMplus < mu1->getSpinStates(); ++iMplus) {
+                EvtDiracSpinor spMplus = mu1->spParent(iMplus);
+                for (int iMminus = 0; iMminus < mu2->getSpinStates(); ++iMminus) {
+                    EvtDiracSpinor spMminus = mu2->spParent(iMminus);
+                    EvtVector4C epsGamma = EvtLeptonVCurrent(spMminus, spMplus);
+                    EvtComplex amp=epsGamma*epsPsi;
+                    amp2 += abs2(amp);
+                };
             };
         };
-        if (iEv <10) {
+        if (iEv < 10) {
             cout << " pPsi=" << pPsi << " m=" << pPsi.mass() << endl;
             cout << " k1=" << k1 << " m=" << k1.mass() << endl;
             cout << " k2=" << k2 << " m=" << k2.mass() << endl;
             Ptot = pPsi + k1 + k2;
             cout << "Ptot=" << Ptot << endl;
             cout << " amp2=" << amp2 << endl;
-            double th = -8 * (k1 * k2 + 2 * mmu * mmu);
+            double th = 4*(2*(pPsi*k1)*(pPsi*k2)/mPsi/mPsi+k1*k2+3*mmu*mmu);
             cout << " amp2/th=" << amp2 / th << endl;
         };
     }
